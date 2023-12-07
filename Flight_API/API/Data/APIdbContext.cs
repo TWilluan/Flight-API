8d40b33aeb92d8b32ee5853d8d7d8069685c1c83
Tuan Vo <tuanvo@Tuans-MacBook-Air.local>
2023-12-07 13:46:10 -0500

    add pacage MySQL

--- Flight_API/API/Data/APIdbContext.cs
+++ Flight_API/API/Data/APIdbContext.cs
@@ -10,11 +10,13 @@ public class APIdbContext : DbContext
     public DbSet<FlightObject> Flights => Set<FlightObject>();
     public DbSet<PassengerObject> Passengers => Set<PassengerObject>();
 
+    public APIdbContext() {}
+
     public APIdbContext(DbContextOptions options) : base(options) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        
 
         modelBuilder.ApplyConfiguration(new FlightConfiguration());
         modelBuilder.ApplyConfiguration(new PassengerConfiguration());
