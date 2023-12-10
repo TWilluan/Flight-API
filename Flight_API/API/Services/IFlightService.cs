4c952ac930a87b849d6206d09e5869eb8ad60b48
Tuan Vo <tuanvo@Tuans-MacBook-Air.local>
2023-12-10 13:58:15 -0500

    implement interface service

--- Flight_API/API/Services/IFlightService.cs
+++ Flight_API/API/Services/IFlightService.cs
@@ -1,9 +1,14 @@
 
 
+using API.DTOs;
+
 namespace API.Service;
 
 public interface IFlightService
 {
-    Task createFlight();
-    Task 
+    Task CreateFlight(Create_FlightDTO flight);
+    Task UpdateFlight(string flight_no, Update_FlightDTO flight);
+    Task<IEnumerable<Reponse_FlightDTO>> GetAllFlight();
+    Task GetFlight(string flight_no);
+    Task DeleteFlight(string flight_no);
 }
--- Flight_API/API/Services/IPassengerService.cs
+++ Flight_API/API/Services/IPassengerService.cs
@@ -1,8 +1,14 @@
 
 
+using API.DTOs;
+
 namespace API.Service;
 
 public interface PassengerService
 {
-    
+    public Task CreatePassenger(Create_PassengerDTO passenger);
+    public Task<IEnumerable<Reponse_PassengerDTO>> GetAllPassenger();
+    public Task GetPassenger(Guid pass_id);
+    public Task UpdatePassenger(Guid pass_id, Update_PassengerDTO passenger);
+    public Task DeletePassenger(Guid pass_id);
 }
