1828feed5526030af1f63c87b87df30a672c4204
Tuan Vo <tuanvo@Tuans-MacBook-Air.local>
2023-12-13 12:37:24 -0500

    remodel api

--- Flight_API/API/Controllers/PassengerController.cs
+++ Flight_API/API/Controllers/PassengerController.cs
@@ -9,30 +9,4 @@ namespace API.Controllers;
 [Route("[controller]")]
 public class PassengerController : ControllerBase
 {
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> CreatePassenger([FromBody] FlightObject flight)
-    {
-        
-        return Ok();
-    }
-
-    [HttpGet]
-    public IActionResult GetPassenger()
-    {
-        return Ok();
-    }
-
-    [HttpPut]
-    public IActionResult UpdatePassenger()
-    {
-        return Ok();
-    }
-
-    [HttpDelete]
-    public IActionResult DeletePassenger()
-    {
-        return Ok();
-    }
 }
